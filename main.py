7e03ee022226ae48e7f958e9d8238787ebd766d9
TeVr42 <tervrb@seznam.cz>
2021-05-05 18:08:27 +0200

    Small refactoring

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ PINK = [
     "k21.png"
     ]
 
+settings = {
+    "light_btn": "simple-button btn btn-outline-light btn-lg",
+    "dark_btn": "simple-button btn btn-light btn-lg",
+    "mode_light_btn": "mode-btn-light btn btn-outline-light",
+    "mode_dark_btn": "mode-btn-dark btn btn-light",
+}
+
 
 def get_colormode(color):
     if color == LIGHT[0]:
@@ -41,35 +48,38 @@ def is_game_on(game):
 
 @app.route('/')
 def home():
-    return render_template("index.html", mode=DARK[0])
+    return render_template("index.html", mode=DARK[0], settings=settings)
 
 
 @app.route('/<color>')
 def colored(color):
-    return render_template("index.html", mode=color)
+    return render_template("index.html", mode=color, settings=settings)
 
 
 @app.route('/<color>/postreh/<int:game>/<int:speed>')
 def hra1(color, game, speed):
     colormode = get_colormode(color)
-    return render_template("game1.html", game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[1])
+    return render_template("game1.html",
+                           game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[1], settings=settings)
 
 
 @app.route('/<color>/presnost/<int:game>/<int:speed>')
 def hra2(color, game, speed):
     colormode = get_colormode(color)
-    return render_template("game2.html", game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[2])
+    return render_template("game2.html",
+                           game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[2], settings=settings)
 
 
 @app.route('/<color>/rychlost/<int:game>')
 def hra3(color, game):
     colormode = get_colormode(color)
-    return render_template("game3.html", game=is_game_on(game), mode=colormode[0], kruhy=colormode[3])
+    return render_template("game3.html",
+                           game=is_game_on(game), mode=colormode[0], kruhy=colormode[3], settings=settings)
 
 
 @app.route('/<color>/info')
 def info(color):
-    return render_template("info.html", mode=get_colormode(color)[0])
+    return render_template("info.html", mode=get_colormode(color)[0], settings=settings)
 
 
 if __name__ == "__main__":
