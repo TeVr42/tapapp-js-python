a041b01e3d9c65dfd65ae59a9d5dae8b86063916
TeVr42 <tervrb@seznam.cz>
2021-05-10 10:44:04 +0200

    Specific Title

--- main.py
+++ main.py
@@ -48,38 +48,38 @@ def is_game_on(game):
 
 @app.route('/')
 def home():
-    return render_template("index.html", mode=DARK[0], settings=settings)
+    return render_template("index.html", mode=DARK[0], settings=settings, title="Domů")
 
 
 @app.route('/<color>')
 def colored(color):
-    return render_template("index.html", mode=color, settings=settings)
+    return render_template("index.html", mode=color, settings=settings, title="Domů")
 
 
 @app.route('/<color>/postreh/<int:game>/<int:speed>')
 def hra1(color, game, speed):
     colormode = get_colormode(color)
     return render_template("game1.html",
-                           game=is_game_on(game), speed=speed, mode=colormode[0], circles_colors=colormode[1], settings=settings)
+                           game=is_game_on(game), speed=speed, mode=colormode[0], circles_colors=colormode[1], settings=settings, title="Postřeh")
 
 
 @app.route('/<color>/presnost/<int:game>/<int:speed>')
 def hra2(color, game, speed):
     colormode = get_colormode(color)
     return render_template("game2.html",
-                           game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[2], settings=settings)
+                           game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[2], settings=settings, title="Přesnost")
 
 
 @app.route('/<color>/rychlost/<int:game>')
 def hra3(color, game):
     colormode = get_colormode(color)
     return render_template("game3.html",
-                           game=is_game_on(game), mode=colormode[0], kruhy=colormode[3], settings=settings)
+                           game=is_game_on(game), mode=colormode[0], kruhy=colormode[3], settings=settings, title="Rychlost")
 
 
 @app.route('/<color>/info')
 def info(color):
-    return render_template("info.html", mode=get_colormode(color)[0], settings=settings)
+    return render_template("info.html", mode=get_colormode(color)[0], settings=settings, title="Info")
 
 
 if __name__ == "__main__":
