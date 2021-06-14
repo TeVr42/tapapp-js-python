87a3616575c94e9156e53bb47b2b4dc61170288b
TeVr42 <tervrb@seznam.cz>
2021-06-14 18:13:11 +0200

    Color Styles to Header, Settings Python file

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 from flask import Flask, render_template
+from settings import SETTINGS, LIGHT, DARK, PINK
 
 app = Flask(__name__)
-
-LIGHT = [
-    "light",
-    ["k2.png", "k3.png", "k4.png", "k5.png", "k9.png", "k10.png", "k16.png", "k17.png", "k18.png", "k19.png", "k21.png"],
-    ["k2.png", "k3.png", "k5.png", "k9.png", "k10.png", "k18.png"],
-    "k18.png"
-    ]
-DARK = [
-    "dark",
-    ["k1.png", "k6.png", "k7.png", "k8.png", "k11.png", "k12.png", "k13.png", "k14.png", "k15.png", "k20.png"],
-    ["k6.png", "k11.png", "k13.png"],
-    "k12.png"
-    ]
-PINK = [
-    "pink",
-    ["k2.png", "k3.png", "k4.png", "k5.png", "k9.png", "k10.png", "k16.png", "k17.png", "k18.png", "k19.png", "k21.png"],
-    ["k2.png", "k3.png", "k5.png", "k9.png", "k10.png", "k18.png"],
-    "k21.png"
-    ]
-
-settings = {
-    "light_btn": "simple-button btn btn-outline-light btn-lg",
-    "dark_btn": "simple-button btn btn-light btn-lg",
-    "mode_light_btn": "mode-btn-light btn btn-outline-light",
-    "mode_dark_btn": "mode-btn-dark btn btn-light",
-}
 
 
 def get_colormode(color):
     for color_mode in [LIGHT, PINK, DARK]:
-        if color == color_mode[0]:
+        if color == color_mode["main"]:
             return color_mode
 
 
@@ -44,38 +19,42 @@ def is_game_on(game):
 
 @app.route('/')
 def home():
-    return render_template("index.html", mode=DARK[0], settings=settings, title="Domů")
+    return render_template("index.html", mode=DARK["main"], settings=SETTINGS, title="Domů", main_colors=DARK["text_bg"])
 
 
 @app.route('/<color>')
 def colored(color):
-    return render_template("index.html", mode=color, settings=settings, title="Domů")
+    return render_template("index.html", mode=color, settings=SETTINGS, title="Domů", main_colors=get_colormode(color)["text_bg"])
 
 
 @app.route('/<color>/postreh/<int:game>/<int:speed>')
 def hra1(color, game, speed):
     colormode = get_colormode(color)
     return render_template("game1.html",
-                           game=is_game_on(game), speed=speed, mode=colormode[0], circles_colors=colormode[1], settings=settings, title="Postřeh")
+                           game=is_game_on(game), speed=speed, mode=colormode["main"], circles_colors=colormode["game1"],
+                           settings=SETTINGS, title="Postřeh", main_colors=colormode["text_bg"])
 
 
 @app.route('/<color>/presnost/<int:game>/<int:speed>')
 def hra2(color, game, speed):
     colormode = get_colormode(color)
     return render_template("game2.html",
-                           game=is_game_on(game), speed=speed, mode=colormode[0], kruhy=colormode[2], settings=settings, title="Přesnost")
+                           game=is_game_on(game), speed=speed, mode=colormode["main"], kruhy=colormode["game2"],
+                           settings=SETTINGS, title="Přesnost", main_colors=colormode["text_bg"])
 
 
 @app.route('/<color>/rychlost/<int:game>')
 def hra3(color, game):
     colormode = get_colormode(color)
     return render_template("game3.html",
-                           game=is_game_on(game), mode=colormode[0], kruhy=colormode[3], settings=settings, title="Rychlost")
+                           game=is_game_on(game), mode=colormode["main"], kruhy=colormode["game3"],
+                           settings=SETTINGS, title="Rychlost", main_colors=colormode["text_bg"])
 
 
 @app.route('/<color>/info')
 def info(color):
-    return render_template("info.html", mode=get_colormode(color)[0], settings=settings, title="Info")
+    return render_template("info.html", mode=get_colormode(color)["main"], settings=SETTINGS, title="Info",
+                           main_colors=get_colormode(color)["text_bg"])
 
 
 if __name__ == "__main__":
---
+++ settings.py
@@ -0,0 +1,30 @@
+LIGHT = {
+    "main": "light",
+    "game1": ["k2.png", "k3.png", "k4.png", "k5.png", "k9.png", "k10.png", "k16.png", "k17.png", "k18.png", "k19.png",
+     "k21.png"],
+    "game2": ["k2.png", "k3.png", "k5.png", "k9.png", "k10.png", "k18.png"],
+    "game3": "k18.png",
+    "text_bg": ["#79a3b1", "#E1E5EA"],
+}
+DARK = {
+    "main": "dark",
+    "game1": ["k1.png", "k6.png", "k7.png", "k8.png", "k11.png", "k12.png", "k13.png", "k14.png", "k15.png", "k20.png"],
+    "game2": ["k6.png", "k11.png", "k13.png"],
+    "game3": "k12.png",
+    "text_bg": ["#cbc5c1", "#4c586f"]
+}
+PINK = {
+    "main": "pink",
+    "game1": ["k2.png", "k3.png", "k4.png", "k5.png", "k9.png", "k10.png", "k16.png", "k17.png", "k18.png", "k19.png",
+              "k21.png"],
+    "game2": ["k2.png", "k3.png", "k5.png", "k9.png", "k10.png", "k18.png"],
+    "game3": "k21.png",
+    "text_bg": ["#DA7F8F", "#FAF3F3"]
+}
+
+SETTINGS = {
+    "light_btn": "simple-button btn btn-outline-light btn-lg",
+    "dark_btn": "simple-button btn btn-light btn-lg",
+    "mode_light_btn": "mode-btn-light btn btn-outline-light",
+    "mode_dark_btn": "mode-btn-dark btn btn-light",
+}
