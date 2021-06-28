9ee4c5d0f8b8d708c1c01ba98c60af93f858c003
TeVr42 <tervrb@seznam.cz>
2021-06-28 21:23:10 +0200

    Settings original colores

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ LIGHT = {
     "main": "light",
     "game1": ["k2.png", "k3.png", "k4.png", "k5.png", "k9.png", "k10.png", "k16.png", "k17.png", "k18.png", "k19.png",
      "k21.png", "k23.png"],
-    "game2": ["k2.png", "k3.png", "k5.png", "k9.png", "k10.png", "k18.png"],
+    "game2": ["k2.png", "k3.png", "k5.png", "k10.png", "k18.png"],
     "game3": "k22.png",
     "text_bg": ["#79a3b1", "#E1E5EA"],
 }
@@ -17,7 +17,7 @@ PINK = {
     "main": "pink",
     "game1": ["k2.png", "k3.png", "k4.png", "k5.png", "k9.png", "k10.png", "k16.png", "k17.png", "k18.png", "k19.png",
               "k21.png", "k23.png"],
-    "game2": ["k2.png", "k3.png", "k5.png", "k9.png", "k10.png", "k18.png"],
+    "game2": ["k2.png", "k3.png", "k5.png", "k10.png", "k18.png"],
     "game3": "k20.png",
     "text_bg": ["#DA7F8F", "#FAF3F3"]
 }
