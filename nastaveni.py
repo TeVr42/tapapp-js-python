eed1198cb570918fb05ca59e774c8f3faf17ede7
Terka <theprincessl261@gmail.com>
2022-01-03 16:02:56 +0100

    Oprava barev pro hru 2

--- nastaveni.py
+++ nastaveni.py
@@ -2,19 +2,19 @@ SVETLY = {
     "hlavni": "svetly",
     "hra1": ["#1F3C5C", "214973", "#214973", "#1E4269", "#A8273C", "#753E47", "#5C1521", "#A84354", "#F53959",
              "#F5AB99", "#F7704F", "ff7e5f", "#204F75", "#8DC7F7"],
-    "hra2": ["#1F3C5C", "#F53959", "#F5AB99", "#ff7e5f", "#204F75", "#8DC7F7"],
+    "hra2": ["#1F3C5C", "#F53959", "#F5AB99", "#8DC7F7"],
     "text_a_pozadi": ["#1F3C5C", "#fff"],
 }
 MODRY = {
     "hlavni": "modry",
     "hra1": ["#fff", "#ff7f60", "#feb47c", "#feb47c", "#f5ab99", "#e1e4e8", "#cbc4c0", "#f6a340", "#e27019", "#f6dcbf", "#e1711b", "#f7a340"],
-    "hra2": ["#fff", "#ff7f60", "#e1e4e8", "#e27019"],
+    "hra2": ["#11CBD7", "#ff7f60", "#fff"],
     "text_a_pozadi": ["#fff", "#142F43"]
 }
 CERVENY = {
     "hlavni": "cerveny",
-    "hra1": ["#F0FFF3", "#F0FFF3", "#11CBD7", "#28C3D4", "#248EA9", "#F30E5C"],
-    "hra2": ["#F0FFF3", "#F0FFF3", "#11CBD7", "#28C3D4", "#248EA9", "#F30E5C"],
+    "hra1": ["#F0FFF3", "#11CBD7", "#28C3D4", "#248EA9", "#F30E5C"],
+    "hra2": ["#F0FFF3", "#11CBD7", "#F30E5C"],
     "text_a_pozadi": ["#F2F2F2", "#852747"]
 }
 
