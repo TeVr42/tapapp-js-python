65d8ae72dd9612cee09ff7cf284ae9e1dd58ac76
Terka <theprincessl261@gmail.com>
2022-04-26 16:55:47 +0200

    test deploy ii

--- nastaveni.py
+++ nastaveni.py
@@ -5,21 +5,18 @@ SVETLY = {
     "hra2": ["#1F3C5C", "#F53959", "#F5AB99", "#8DC7F7"],
     "text_a_pozadi": ["#1F3C5C", "#fff"],
 }
-
 MODRY = {
     "hlavni": "modry",
     "hra1": ["#fff", "#ff7f60", "#feb47c", "#feb47c", "#f5ab99", "#e1e4e8", "#cbc4c0", "#f6a340", "#e27019", "#f6dcbf", "#e1711b", "#f7a340"],
     "hra2": ["#11CBD7", "#ff7f60", "#fff"],
     "text_a_pozadi": ["#fff", "#142F43"]
 }
-
 CERVENY = {
     "hlavni": "cerveny",
     "hra1": ["#F0FFF3", "#11CBD7", "#28C3D4", "#248EA9", "#F30E5C"],
     "hra2": ["#F0FFF3", "#11CBD7", "#F30E5C"],
     "text_a_pozadi": ["#F2F2F2", "#852747"]
 }
-
 NASTAVENI = {
     "svetly_btn": "zakladni-tlacitko btn btn-outline-light btn-lg",
     "tmavy_btn": "zakladni-tlacitko btn btn-light btn-lg",
